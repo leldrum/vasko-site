d298602ed3cfd78adfe24805da311ab4a2947939
Lélyan <lelyanravida@gmail.com>
2026-07-18 23:33:45 +0200

    Revert "conf: environment en prod"
    This reverts commit 352154d9d8e058eff0e9178cfbc4b269617263e0.

--- src/app/services/media-service.ts
+++ src/app/services/media-service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { environment } from "../../environments/environment";
+import { environment } from "../../environments/environment.prod";
 import { MediaCreate, Media } from "../models/media-model";
 
 @Injectable({ providedIn: 'root' })
